d4cb8ebcedbc6c8cf26aa3c675838cb8e95273e6
Alexander Westberg-Bladh <alexander@challengermode.com>
2023-12-08 17:05:09 +0100

    :tada: Add support for Challengermode demos

--- pkg/api/constants/demo_source.go
+++ pkg/api/constants/demo_source.go
@@ -7,17 +7,18 @@ func (source DemoSource) String() string {
 }
 
 const (
-	DemoSourceUnknown    DemoSource = "unknown"
-	DemoSourceValve      DemoSource = "valve"
-	DemoSourceESEA       DemoSource = "esea"
-	DemoSourceFaceIt     DemoSource = "faceit"
-	DemoSourceEbot       DemoSource = "ebot"
-	DemoSourceCEVO       DemoSource = "cevo"
-	DemoSourceESL        DemoSource = "esl"
-	DemoSourcePopFlash   DemoSource = "popflash"
-	DemoSourceEsportal   DemoSource = "esportal"
-	DemoSourceFastcup    DemoSource = "fastcup"
-	DemoSourceGamersclub DemoSource = "gamersclub"
+	DemoSourceUnknown        DemoSource = "unknown"
+	DemoSourceValve          DemoSource = "valve"
+	DemoSourceESEA           DemoSource = "esea"
+	DemoSourceFaceIt         DemoSource = "faceit"
+	DemoSourceEbot           DemoSource = "ebot"
+	DemoSourceCEVO           DemoSource = "cevo"
+	DemoSourceChallengermode DemoSource = "challengermode"
+	DemoSourceESL            DemoSource = "esl"
+	DemoSourcePopFlash       DemoSource = "popflash"
+	DemoSourceEsportal       DemoSource = "esportal"
+	DemoSourceFastcup        DemoSource = "fastcup"
+	DemoSourceGamersclub     DemoSource = "gamersclub"
 )
 
 var SupportedDemoSources = []DemoSource{
